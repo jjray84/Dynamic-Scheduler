ae72b962a2a226b43c2019c0a20e72afe71f6d9e
Melona-png <118491178+Melona-png@users.noreply.github.com>
2023-01-14 10:49:06 -0500

    fixing missing div and organizing right columns and rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,87 @@
 var today = dayjs();
 var notesList = [];
-var notesArea = $('#notesArea');
-var addNoteBtn = $('#addNoteBtn')
+var notesArea = $("#notesArea");
+var addNoteBtn = $("#addNoteBtn");
 
 //Populates the notes area with the saved notes.
-function renderNotes () {
-    notesArea.innerHTML = '';  
-    notesList = notesFromStorage();
-    // notesList = [1, 2, 3]; 
-    for (var i = 0; i<notesList.length; i++) {
-        var entry = notesList[i];
-        console.log(entry);
-        var noteItem = $(`<p>${entry.noteTitle}: ${entry.noteDetails}</p>`);
-        //noteItem.innerHTML = `${entry.noteTitle}`;
-        console.log(noteItem);
-        notesArea.append(noteItem);
-    }
-
-  };
+function renderNotes() {
+  notesArea.innerHTML = "";
+  notesList = notesFromStorage();
+  // notesList = [1, 2, 3];
+  for (var i = 0; i < notesList.length; i++) {
+    var entry = notesList[i];
+    console.log(entry);
+    var noteItem = $(`<p>${entry.noteTitle}: ${entry.noteDetails}</p>`);
+    //noteItem.innerHTML = `${entry.noteTitle}`;
+    console.log(noteItem);
+    notesArea.append(noteItem);
+  }
+}
 
 //Logic to run when the page initializes
 function init() {
-renderNotes();
-};
+  renderNotes();
+}
 
-console.log(today.format('MMM D, YYYY'));
+console.log(today.format("MMM D, YYYY"));
 
-
-
-document.getElementById('newEmployee').addEventListener('click', function() {
-    localStorage.setItem('info',
-    JSON.stringify({ name: "name", status: "FT/PT", daysAvailable: "Su, M, T, W, Th, F, Sa"})
-    );
-    updateUI();
-})
+document.getElementById("newEmployee").addEventListener("click", function () {
+  localStorage.setItem(
+    "info",
+    JSON.stringify({
+      name: "name",
+      status: "FT/PT",
+      daysAvailable: "Su, M, T, W, Th, F, Sa",
+    })
+  );
+  updateUI();
+});
 
 //Event Listener for the add note button
 addNoteBtn.on("click", addNote);
 
 //Function for when a user adds a new note
-function addNote (event) {
+function addNote(event) {
   event.preventDefault();
   var newNote = {
-    noteTitle: $('#noteTitle').val(),
-    noteDetails: $('#noteDetails').val()
+    noteTitle: $("#noteTitle").val(),
+    noteDetails: $("#noteDetails").val(),
   };
   console.log(newNote);
   var storedNotesList = notesFromStorage();
   console.log(storedNotesList);
   if (storedNotesList == null) {
-    notesList[newNote]
+    notesList[newNote];
   } else {
     notesList = storedNotesList;
-    notesList.push(newNote)
+    notesList.push(newNote);
   }
   console.log(storedNotesList);
-  localStorage.setItem('notes', JSON.stringify(notesList));
+  localStorage.setItem("notes", JSON.stringify(notesList));
   renderNotes();
 }
 
-function notesFromStorage () {
+function notesFromStorage() {
   return JSON.parse(localStorage.getItem("notes"));
 }
 
 //Displays the add note dialog box
-$( function() {
-  $( "#dialog" ).dialog({
+$(function () {
+  $("#dialog").dialog({
     autoOpen: false,
     show: {
       effect: "blind",
-      duration: 1000
+      duration: 1000,
     },
     hide: {
       effect: "explode",
-      duration: 1000
-    }
+      duration: 1000,
+    },
   });
 
-  $( "#noteDialog" ).on( "click", function() {
-    $( "#dialog" ).dialog( "open" );
+  $("#noteDialog").on("click", function () {
+    $("#dialog").dialog("open");
   });
-} );
+});
 
 init();
